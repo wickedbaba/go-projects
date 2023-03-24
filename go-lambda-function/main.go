edfb07ae755f02250e2078f2d9181fe1b0537080
wickedbaba <aryamansinghranaasr@gmail.com>
2023-03-24 11:18:42 +0530

    Finished working on AWS Lmabda with Golang

--- go-lambda-function/main.go
+++ go-lambda-function/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 var pl = fmt.Println
 var pf = fmt.Printf
 
-func main() {
+type MyEvent struct {
+	Name string `json:"What is your name?`
+	Age  int    `json:"How old are you?`
+}
+type MyResponse struct {
+	Message string `json:"Answer:`
+}
 
+func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
+	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+}
+
+func main() {
+	lambda.Start(HandleLambdaEvent)
 }
